798803c2bb7da5e64cf1ce728363243a498e6c38
p-ryder <superdeveloper774@gmail.com>
2023-07-11 09:10:56 +1000

    Update: Added live lemonsqueezy buttons.

--- components/Banner/HomeBanner.js
+++ components/Banner/HomeBanner.js
@@ -38,7 +38,7 @@ export const HomeBanner = () => {
                             <Button href="#features">Features</Button>
                             <a
                                 role="button"
-                                href="https://avenuelabs.lemonsqueezy.com/checkout/buy/70fe6994-64d0-4238-9151-010aabf97261?embed=1"
+                                href="https://avenuelabs.lemonsqueezy.com/checkout/buy/df8ccd46-fa63-4384-b2aa-c4c9b8ad3ef0?embed=1"
                                 className="btn btn--secondary lemonsqueezy-button"
                             >
                                 Get Template
--- components/Header/Header.js
+++ components/Header/Header.js
@@ -31,7 +31,7 @@ export const Header = () => {
                     <ButtonGroup className="hidden md:block">
                         <a
                             role="button"
-                            href="https://avenuelabs.lemonsqueezy.com/checkout/buy/70fe6994-64d0-4238-9151-010aabf97261?embed=1"
+                            href="https://avenuelabs.lemonsqueezy.com/checkout/buy/df8ccd46-fa63-4384-b2aa-c4c9b8ad3ef0?embed=1"
                             className="btn btn--secondary lemonsqueezy-button ml-4"
                         >
                             Get Template
